81be6a8769d8202aa513f0d914daf81f48b385c0
jjh770 <whdgur0068@gmail.com>
2025-11-11 13:41:53 +0900

    Feat : 배경오브젝트 시차 스크롤링 적용

--- skky_2dshooting/Assets/02.Scripts/Environment/BackgroundScroll.cs
+++ skky_2dshooting/Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -6,11 +6,17 @@ public class BackgroundScroll : MonoBehaviour
 
     // 필요 속성
     // - 머터리얼
+    public Material BackgroundMaterial;
     // - 스크롤 속도
+    public float ScrollSpeed = 0.1f;
+
+
 
     void Update()
     {
         // 방향을 구한다.
+        Vector2 direction = Vector2.up; 
         // 움직인다. (스크롤)
+        BackgroundMaterial.mainTextureOffset += direction * ScrollSpeed * Time.deltaTime;
     }
 }
